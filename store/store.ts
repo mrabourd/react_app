2fee8ba9b46c90a8aca69cc0f045ce66fa1978c8
vboxuser <vboxuser@vmprojet.myguest.virtualbox.org>
2025-09-16 22:37:51 +0200

    fix: remove useless lines

--- store/store.ts
+++ store/store.ts
@@ -10,10 +10,6 @@ const counterSlice = createSlice({
   },
   reducers: {
     incremented: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1
     },
     decremented: state => {
@@ -37,10 +33,3 @@ export type AppDispatch = typeof store.dispatch;
 
 store.subscribe(() => (store.getState()))
 
-// Still pass action objects to `dispatch`, but they're created for us
-store.dispatch(incremented())
-// {value: 1}
-store.dispatch(incremented())
-// {value: 2}
-store.dispatch(decremented())
-// {value: 1}
